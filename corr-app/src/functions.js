0d8a7503965aef9106c2aa2ef9ed15224e66df06
Justin Olson <jsolson4@wisc.edu>
2024-06-13 22:52:07 -0400

    add refreshTextsAndNodes

--- corr-app/src/functions.js
+++ corr-app/src/functions.js
@@ -5,51 +5,7 @@ import './ticker_styles.css';
 
 // Three function that change the tooltip when user hover / move / leave a cell
 
-// define what happens on mouse over event
-// Mouseover event
-export function mouseover(event, d) {
-    console.log("mouseover");
 
-    // Get pointer position relative to the document
-    //const [mouseX, mouseY] = d3.pointer(event);
-    console.log(d.name, d.Correlation);
-
-    // Select and style the tooltip
-    d3.select("#tooltip")
-        .html(`Security: ${d.name}<br>Correlation: ${d.Correlation}`)
-        .style("left", `${d.x}px`) // Offset the tooltip slightly to the right of the cursor
-        .style("top", `${d.y + 10}px`) // Offset the tooltip slightly below the cursor
-        .classed("visible", true); // Add the visible class to show the tooltip
-}
-
-// export function mouseover(event, d) {
-//     console.log("mouseover", event.currentTarget)
-//     console.log(event)
-//     console.log("d:", d)
-//     // Get pointer position
-//     //const xy = d3.pointer(event, svg.node());
-//     const xy = d3.pointer(event, event.currentTarget);
-//     console.log(d.name, d.Correlation)
-//     d3.select("#tooltip")
-//         .style("opacity", 1)
-//         .html(`Security:${d.name}<br>Correlation: ${d.Correlation}`)
-//         .style("left", (d.x) + "px")
-//         .style("top", (d.y) + "px");
-    
-// };
-
-// define what happens when the mouse leaves the svg
-// export function mouseleave(event, d) {
-//     console.log("mouse leave")
-//     d3.select("#tooltip")
-//         .style("opacity", 0)
-//   };
-
-// Mouseleave event
-export function mouseleave(event, d) {
-    d3.select("#tooltip")
-      .classed("visible", false); // Remove the visible class to hide the tooltip
-}
 
 
 // function to calculate the node radius
@@ -59,33 +15,20 @@ export function calcRadius(nodeScale, correlation){
 };
 
 
-// Function to update forceCollide after node radius change
 export function updateForceCollide(simulation, nodeScale) {
+    console.log("Updating force collide");
+    
     // Update the radius accessor function of forceCollide
-    // simulation.force("collide", d3.forceCollide(function(d){
-    //             return radius; //d.Correlation
-    //         })
-    //         .strength(0.8));
+    simulation.force("collide", d3.forceCollide().radius(d => {
+        const radius = nodeScale(Math.abs(d.Correlation));
+        return radius + 2; // Adding padding to avoid overlap
+    }).strength(0.8));
 
-   console.log("run update force collide")
-   
-    // Update the radius accessor function of forceCollide
-   simulation.force("collide", 
-                    d3.forceCollide(function(d){
-                        console.log("d:", d)
-                    return nodeScale(Math.abs(d.Correlation))
-                    ; //d.Correlation
-    })
-    .strength(0.8));
+    // Restart the simulation to apply the changes
+    simulation.alpha(0.3).restart();
+    //console.log("Simulation alpha:", simulation.alpha());
 
-    // restart the simulation to apply the changes
-    simulation.alpha(1).restart();
-
-};
-
-// update simulation
-export function restartSimulation(simulation){
-    simulation.alpha(1).restart();
+    return simulation;
 }
 
 
@@ -108,9 +51,6 @@ export function resetNodeCorrelations(graphData, origNodes, d, circles, nodeScal
         // update circle radius size to be that same as initally specified
         updateCircles.call(this, radius, newCorr);
     })
-    
-    // update simulation to reflect new node sizes
-    updateForceCollide(simulation, nodeScale);
 };
 
 
@@ -125,11 +65,6 @@ export function updateCircles(radius, correlation){
     });
 };
 
-// function to update the simulation value
-export function updateSimulation(){
- // todo
-};
-
 
 // function update node corr
 export function updateNodeCorr(nodes, nodeName, newCorr){
@@ -140,11 +75,163 @@ export function updateNodeCorr(nodes, nodeName, newCorr){
     return nodes
 };
 
+// SOLUTION TO MY ISSUE WAS THAT I NEEDED TO REFRESH TEXTSANDNODES
+function refreshTextsAndNodes(svg, nodes, nodeScale) {
+    const textsAndNodes = svg.selectAll("g")
+        .data(nodes);
 
-// updateNodeRadiusToLinkCorr
+    textsAndNodes.select("circle")
+        .attr("r", d => nodeScale(Math.abs(d.Correlation)))
+        .attr("fill", d => d.Correlation < 0 ? "#A9A9A9" : "#327FB9");
+
+    textsAndNodes.select("text")
+        .text(d => d.name);
+
+    textsAndNodes.attr("transform", d => `translate(${d.x}, ${d.y})`);
+};
+
+
+export function clicked(d, origNodes, graphData, circles, nodeScale, simulation, myHeading, defaultHeading, svg) {
+
+    const circle = d3.select(this).select("circle");
+    const text = d3.select(this).select("text");
+
+    const selectedNode = d3.selectAll("circle.selectedNode");
+    const selectedText = d3.selectAll("text.selectedText");
+
+    circle.attr("class", "selectedNode");
+    text.attr("class", "selectedText");
+
+    myHeading.text(`Correlations to ${d.name}`);
+    
+    if (circle.size() === 1 && selectedNode.size() === 0) {
+      console.log("start resize")
+      
+      // circles to textsAndNodes
+      updateNodeRadiusToLinkCorr(d, circles, origNodes, graphData, nodeScale, simulation);
+      console.log("graphData in clicked after corr update:", graphData)
+    }
+
+    if (selectedNode.size() === 1) {
+      selectedNode.attr("class", "node");
+      selectedText.attr("class", "text");
+    }
+
+    const selectedNodeEndState = d3.selectAll("circle.selectedNode");
+
+    if (circle.size() === 1 && selectedNode.size() === 1 && selectedNodeEndState.size() === 1) {
+      // circles to textAndNodes
+      updateNodeRadiusToLinkCorr(d, circles, origNodes, graphData, nodeScale, simulation);
+      console.log("graphData in clicked after corr update:", graphData)
+    }
+
+    if (selectedNodeEndState.size() === 0) {
+      myHeading.text(defaultHeading);
+      
+      // circles to textAndNodes
+      resetNodeCorrelations(graphData, origNodes, d, circles, nodeScale, simulation, myHeading, defaultHeading);
+    }
+    // DEV:add final sim reset (can delete...)
+    console.log("run force collide")
+    simulation = updateForceCollide(simulation, nodeScale);
+
+    // Refresh textsAndNodes to reflect updated data
+    refreshTextsAndNodes(svg, graphData.nodes, nodeScale);
+
+  };
+
+
+  export function ticked(textsAndNodes) {
+    textsAndNodes.attr("transform", d => `translate(${d.x}, ${d.y})`);
+  };
+
+
+
+
+
+//   export function updateNodeRadiusToLinkCorr(d, circles, origNodes, graphData, nodeScale, simulation) {
+//     console.log("Begin updateNodeRadiusToLinkCorr");
+    
+//     const relevantLinks = graphData.links.filter(link => link.source === d.name);
+//     const correlationArray = relevantLinks.map(link => link.correlation);
+
+//     circles.each(function(e) {
+//         let newCorr;
+//         if (d.name === e.name) {
+//             newCorr = origNodes.find(node => node.name === d.name).Correlation;
+//         } else {
+//             const connectedLink = graphData.links.find(l => l.source === d.name && l.target === e.name);
+//             newCorr = connectedLink ? connectedLink.correlation : 0;
+//         }
+
+//         graphData.nodes = updateNodeCorr(graphData.nodes, e.name, newCorr);
+//         const radius = calcRadius(nodeScale, newCorr);
+//         updateCircles.call(this, radius, newCorr);
+//     });
+
+//     simulation = updateForceCollide(simulation, nodeScale);
+// };
+
+// export function mouseover(event, d) {
+//     console.log("mouseover", event.currentTarget)
+//     console.log(event)
+//     console.log("d:", d)
+//     // Get pointer position
+//     //const xy = d3.pointer(event, svg.node());
+//     const xy = d3.pointer(event, event.currentTarget);
+//     console.log(d.name, d.Correlation)
+//     d3.select("#tooltip")
+//         .style("opacity", 1)
+//         .html(`Security:${d.name}<br>Correlation: ${d.Correlation}`)
+//         .style("left", (d.x) + "px")
+//         .style("top", (d.y) + "px");
+    
+// };
+
+
+// // Function to update forceCollide after node radius change
+// export function updateForceCollide(simulation, nodeScale) {
+//     // Update the radius accessor function of forceCollide
+//     // simulation.force("collide", d3.forceCollide(function(d){
+//     //             return radius; //d.Correlation
+//     //         })
+//     //         .strength(0.8));
+
+//    console.log("run update force collide")
+   
+//     // Update the radius accessor function of forceCollide
+//    simulation.force("collide", 
+//                     d3.forceCollide().radius(function(d){
+//                         if (d.name === 'SSSS'){
+//                             console.log("d:", d)
+//                             console.log("correlation:", d.Correlation)
+//                             console.log("radius:", nodeScale(Math.abs(d.Correlation)))
+//                         }
+                        
+//                     return nodeScale(Math.abs(d.Correlation))
+//                     ; //d.Correlation
+//     })
+//     .strength(0.8));
+
+//     // restart the simulation to apply the changes
+//     simulation.alpha(0.3).restart();
+//     console.log("simulation alpha:", simulation.alpha())
+
+//     return simulation
+// };
+
+// // update simulation
+// export function restartSimulation(simulation){
+//     simulation.alpha(1).restart();
+// }
+
+
+
+
+// updateNodeRadiusToLinkCorr (original)
 //
 // Define a function to update node radii based on link correlation
-export function updateNodeRadiusToLinkCorr(d, circles, origNodes, graphData, nodeScale, simulation) { // updateNodeRadii
+export function updateNodeRadiusToLinkCorr(d, circles, origNodes, graphData, nodeScale, simulation) {
 
     // get all relevant links
     console.log("begin updateNodeRadiusToLinkCorr")
@@ -152,66 +239,50 @@ export function updateNodeRadiusToLinkCorr(d, circles, origNodes, graphData, nod
     console.log("circles:", circles)
     console.log("graphData:", graphData)
     const relevantLinks = graphData.links.filter(link => link.source === d.name)
-
+    console.log("relevant links:", relevantLinks)
+    
     // extract corrrelation values into an array
-    const correlationArray = relevantLinks.map(link => link.correlation)
+    //const correlationArray = relevantLinks.map(link => link.correlation)
 
     circles.each(function(e) {
-
+        console.log("e:", e)
         // size selected node
         if (d.name === e.name){
-            
             // find the new correlation value
-            const newCorr = origNodes.filter(node => node.name === d.name)[0].Correlation
+            const newCorr = origNodes.filter(node => node.name === d.name)[0].Correlation;
 
             // update correlation value in graphData.nodes
-            //graphData.nodes.filter(node => node.name === d.name)[0].Correlation = newCorr
-            graphData.nodes = updateNodeCorr(graphData.nodes, d.name, newCorr)
-
+            updateNodeCorr(graphData.nodes, d.name, newCorr);
+            
             // get node radius
-            const radius = calcRadius(nodeScale, newCorr)
+            const radius = calcRadius(nodeScale, newCorr);
 
             // update the circle sizes
             updateCircles.call(this, radius, newCorr);
-        }
-
-        // size all other nodes
-        else {
+        
+        } else { // size all other nodes
+        
             // for non-selected nodes, update with correlation value
             const connectedLinks = graphData.links.filter(l => l.source === d.name && l.target === e.name);
             
             // new corr is extracted from the set of links
-            const newCorr = connectedLinks[0].correlation
+            const newCorr = connectedLinks[0].correlation;
             
             // get node radius
-            const radius = calcRadius(nodeScale, newCorr)
+            const radius = calcRadius(nodeScale, newCorr);
 
             // update circles
-            updateCircles.call(this, radius, newCorr)
+            updateCircles.call(this, radius, newCorr);
 
             // update node corr
-            graphData.nodes = updateNodeCorr(graphData.nodes, e.name, newCorr);
-            
-            // // Find the index of the node with name 'e.name'
-            // const index = graphData.nodes.findIndex(node => node.name === e.name)
-
-            // //update node corr
-            // graphData.nodes[index].Correlation = newCorr
+            updateNodeCorr(graphData.nodes, e.name, newCorr);
+            console.log("node corr post update:", graphData.nodes)
         };
-
-        // update simulation to reflect new node sizes
-        updateForceCollide(simulation, nodeScale);
     });
 
     // restart simulation to apply changes (update node radii sizes)
-    restartSimulation(simulation);
+    //restartSimulation(simulation);
+    updateForceCollide(simulation, nodeScale);
 };
-
-
-// export function dragged(event, d) {
-//   d.fx = event.x;
-//   d.fy = event.y;
-// };
-
 
 
